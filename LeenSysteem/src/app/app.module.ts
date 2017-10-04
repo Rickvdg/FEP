013066b060703cb8e3a393506d0a0ac267f34fb2
Rick <rickvdg97@gmail.com>
2017-10-04 20:09:54 +0200

    Added navbar with routing

--- LeenSysteem/src/app/app.module.ts
+++ LeenSysteem/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NotFoundComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]

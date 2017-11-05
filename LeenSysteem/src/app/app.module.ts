4cd2bea9b5ec3391dadad2226b802e1973d525a6
K van Wijngaarden <degeer33>
2017-11-05 21:59:29 +0100

    -Model werkt -product naar basket werkt -basic validation

--- LeenSysteem/src/app/app.module.ts
+++ LeenSysteem/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginModule } from './login/login.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatDialogModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
@@ -13,9 +13,11 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationService } from "./authentication.service";
 import { AuthGuardService } from './auth-guard.service';
-import { CatalogComponent } from './catalog/catalog.component';
+import { BasketQtyDialog, CatalogComponent } from './catalog/catalog.component';
 // import { ReservationsComponent } from './reservations/reservations.component';
 import { AngularFireModule } from "angularfire2";
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -41,6 +43,7 @@ export const firebaseConfig = {
     HomeComponent,
     LoginComponent,
     CatalogComponent,
+    BasketQtyDialog,
     // ReservationsComponent
   ],
   imports: [
@@ -50,8 +53,12 @@ export const firebaseConfig = {
     NgbModule.forRoot(),
     AngularFireDatabaseModule,
     BrowserAnimationsModule,
-    MatButtonModule, MatCheckboxModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    MatButtonModule, MatCheckboxModule, MatDialogModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule, ReactiveFormsModule
+  ],
+  entryComponents: [
+    BasketQtyDialog
   ],
   providers: [
     AuthenticationService,
--- LeenSysteem/src/app/catalog/catalog.component.ts
+++ LeenSysteem/src/app/catalog/catalog.component.ts
@@ -1,23 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+///<reference path="../../../node_modules/@angular/forms/src/validators.d.ts"/>
+import { Component, OnInit, Inject } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
   styleUrls: ['./catalog.component.css']
 })
+
 export class CatalogComponent implements OnInit {
   productList: FirebaseListObservable<any[]>;
+  basketList: Array<any> = [];
 
-  constructor(
-    public database: AngularFireDatabase
-  ) {
+  product: string;
+  qty: number = 0;
+  maxQty: number = 0;
+
+  minMaxControl = new FormControl("", [Validators.max(20), Validators.min(0)]);
+
+  constructor( public database: AngularFireDatabase, public dialog: MatDialog) {
     this.productList = this.database.list('/catalog-products');
-
 
   }
 
   ngOnInit() {
   }
 
+  addToBasket() {
+    let tuple = [this.product, this.qty];
+    this.basketList.push(tuple);
+    console.log(this.basketList);
+  }
+
+  openDialog(productName: string, maxQty: number) {
+    this.product = productName;
+    this.qty = 0;
+    this.maxQty = maxQty;
+
+    let dialogRef = this.dialog.open(BasketQtyDialog, {
+      width: '500px',
+      data: { productName: this.product, qty: this.qty, maxQty: this.maxQty}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.qty = result;
+
+      console.log(this.qty > 0);
+
+      if(this.qty != 0 && this.qty != null){
+        this.addToBasket()
+      }
+
+    });
+  }
+}
+
+@Component({
+  selector: 'basket-qty-dialog',
+  templateUrl: 'basket-qty-dialog.html',
+  styleUrls: ['./catalog.component.css']
+})
+export class BasketQtyDialog {
+  minMaxControl = new FormControl("", [ Validators.max(this.data.maxQty),
+                                        Validators.min(1),
+                                        Validators.required]);
+
+  constructor( public dialogRef: MatDialogRef<BasketQtyDialog>, @Inject(MAT_DIALOG_DATA) public data: any) {  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }

2c7fa9e400850ecb01376442a4caee0b1fd97da3
Martijn <martijn.dull@student.hu.nl>
2017-11-05 21:22:56 +0100

    app.module.ts form imports

--- LeenSysteem/src/app/app.module.ts
+++ LeenSysteem/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AngularFireDatabaseModule } from 'angularfire2/database-deprecated';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from "./not-found/not-found.component";
@@ -13,15 +15,18 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationService } from "./authentication.service";
 import { AuthGuardService } from './auth-guard.service';
+import { ProductService} from "./product/product.service";
 import { CatalogComponent } from './catalog/catalog.component';
 import { ReservationsComponent } from './reservations/reservations.component';
 import {AngularFireModule} from "angularfire2";
+import { CatalogProductComponent } from './catalog-product/catalog-product.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'catalog', component: CatalogComponent, canActivate: [AuthGuardService]},
   { path: 'reservations', component: ReservationsComponent, canActivate: [AuthGuardService]},
+  { path: 'products', component: CatalogProductComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ]
@@ -41,10 +46,13 @@ export const firebaseConfig = {
     HomeComponent,
     LoginComponent,
     CatalogComponent,
-    ReservationsComponent
+    ReservationsComponent,
+    CatalogProductComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes, {enableTracing: true}),
     LoginModule,
     NgbModule.forRoot(),
@@ -55,7 +63,8 @@ export const firebaseConfig = {
   ],
   providers: [
     AuthenticationService,
-    AuthGuardService
+    AuthGuardService,
+    ProductService
   ],
   bootstrap: [AppComponent]
 })
